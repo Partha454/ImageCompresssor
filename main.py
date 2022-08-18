ff0cd9915c8a858bb76afaa0aec91b4d0b750018
Partha454 <69978726+Partha454@users.noreply.github.com>
2022-08-18 10:35:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#Resizes an image and keeps aspect ratio. Set mywidth to the desired with in pixels.
+
+import PIL
+from PIL import Image
+
+mywidth = 1614
+
+img = Image.open('C:/Users/parth/OneDrive/Documents/IMagecompressor/1.jpg')
+wpercent = (mywidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((mywidth,hsize), PIL.Image.ANTIALIAS)
+img.save('C:/Users/parth/OneDrive/Documents/IMagecompressor/11.jpg')
