b82ce2923d75b5d27f355ac259301fdf11bdd469
Partha454 <69978726+Partha454@users.noreply.github.com>
2022-08-18 10:36:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 mywidth = 1614
 
-img = Image.open('C:/Users/parth/OneDrive/Documents/IMagecompressor/1.jpg')
+img = Image.open('C:/Users/parth/OneDrive/Documents/IMagecompressor/1.jpg')//imagepath
 wpercent = (mywidth/float(img.size[0]))
 hsize = int((float(img.size[1])*float(wpercent)))
 img = img.resize((mywidth,hsize), PIL.Image.ANTIALIAS)
-img.save('C:/Users/parth/OneDrive/Documents/IMagecompressor/11.jpg')
+img.save('C:/Users/parth/OneDrive/Documents/IMagecompressor/11.jpg')//new image path
